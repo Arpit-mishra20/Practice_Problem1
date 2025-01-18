33ca79a8db51be8e96801a2b996aed0bf4f19c22
Arpit-mishra20 <arpit.202033@gmail.com>
2025-01-18 15:36:28 +0530

    added random program

---
+++ random.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class random{
+public static void main(String[] args){
+Random random=new Random();
+int[] numbers=new int[5];
+for(int i=0;i<numbers.length;i++){
+numbers[i]=100+random.nextInt(900);
+System.out.print(numbers[i]+" ");
+System.out.println();
+int min=numbers[0];
+int max=numbers[0];
+for(int num:numbers){
+if(num<min){
+min=num;
+}
+if(num>max){
+max=num;
+}
+}
+System.out.println("Min"+min);
+System.out.println("Max"+max);
+}
+}
+}
